0b1be35390fee4908dcd3e48a27c6548fc142b50
nohack <hey1987808049@gmail.com>
2025-06-16 22:23:18 +0900

    fix: jxa not portable

--- src/low_level/execute_jxa.rs
+++ src/low_level/execute_jxa.rs
@@ -1,15 +1,29 @@
-use std::process::Command;
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
 
 use serde_json::Value;
 
+static SCRIPT: &[u8] = include_bytes!("nowPlaying.jxa");
+
 pub fn get_raw_info() -> Option<Value> {
-    let output = Command::new("osascript")
+    let mut child = Command::new("osascript")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
         .arg("-l")
         .arg("JavaScript")
-        .arg("src/low_level/nowPlaying.jxa")
-        .output()
-        .expect("Failed to execute command");
+        .spawn()
+        .expect("Failed to spawn command");
 
+    child
+        .stdin
+        .as_mut()
+        .expect("Failed to open stdin")
+        .write_all(SCRIPT)
+        .expect("Failed to write to stdin");
+
+    let output = child.wait_with_output().expect("Failed to read stdout");
     if !output.status.success() {
         return None;
     }
